7e5835efbbb9fbd8151dd20600e766c45d8e79ea
jdar22169 <mmow313@gmail.com>
2016-06-02 14:39:36 -0700

    change in server

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.use((err,req,res,next) => {
   res.status(404).json({message:err.message});
   next(err);
 });
-app.listen(3000, () => console.log('listening on 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('listening on ' + process.env.PORT || 3000));
